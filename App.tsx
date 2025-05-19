529e1612dec3b0931409e5d1daa1c65fac588b55
Macbook <macbook@Asint.local>
2025-05-18 21:15:45 -0300

    personalizando a statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 
-import {Text, View } from 'react-native';
+import {StatusBar, Text, View } from 'react-native';
 import {useFonts,Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/roboto'
 
 export default function App() {
 
   const [fontsLoading] = useFonts({ Roboto_700Bold, Roboto_400Regular})
   return (
-    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#202024'}}>
+     <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
      { fontsLoading ? <Text>Home</Text> : <View />}
     </View>
   );
